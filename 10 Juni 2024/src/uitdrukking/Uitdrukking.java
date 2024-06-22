b79a0e40b3ff026b5cd30de6e2ae4458f09a89f6
Roan <roan1segers@gmail.com>
2024-06-22 20:12:26 +0200

    fix

--- 10 Juni 2024/src/uitdrukking/Uitdrukking.java
+++ 10 Juni 2024/src/uitdrukking/Uitdrukking.java
@@ -6,7 +6,7 @@ public abstract class Uitdrukking {
 	 * @pre | v != null
 	 * @post | result >= 0
 	 * @post | result == ((this instanceof OptellingUitdrukking ou )? (ou.getLinks().aantalVoorkomens(v) + ou.getRechts().aantalVoorkomens(v)): (this instanceof VariabeleUitdrukking vu ? (v.equals(vu.getVariabele()) ? 1: 0) : 0))
-	 * Omdat deze functie in de abstract klasse staat kunnnen alle andere klassen deze functie gebruiken, en eventueel ook overriden. Elke klasse moet strengere (of even strenge) pre en post condities hebben voor de functie.
+	 * Omdat dit een abtracte functie is moeten alle deelklassen deze functie implementeren (en overriden). Elke klasse moet strengere (of even strenge) pre en post condities hebben voor de functie.
 	 */
 	public abstract int aantalVoorkomens(String v);
 	
